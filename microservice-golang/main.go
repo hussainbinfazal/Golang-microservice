9e42abc0f7fe25431c57709da98f6b20c833e4c3
hussainbinfazal <mdhussain0610@gmail.com>
2025-08-09 18:40:53 +0530

    fiber & air package initialized

--- microservice-golang/main.go
+++ microservice-golang/main.go
@@ -1,7 +1,14 @@
-
-
 package main
-import "fmt"
-func main(){
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
 	fmt.Println("hello Hussain ,Welcome to the Micoservice Backend")
+	app := fiber.New()
+	log.Fatal(app.Listen(":3000"))
 }
