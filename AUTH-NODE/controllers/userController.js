eb83c4b10f6b918991f23101f53e6c84fd8b6cef
hussainbinfazal <mdhussain0610@gmail.com>
2025-08-14 02:43:01 +0530

    added controller in auth service

--- AUTH-NODE/controllers/userController.js
+++ AUTH-NODE/controllers/userController.js
@@ -5,7 +5,7 @@ import { auth } from '../auth.js';
 const prisma = new PrismaClient();
 export const createUser = (req, res) => {
     try {
-        
+
         const { email, password, name, phone, role, profilePicture } = req.body;
         if (!email || !password) {
             return res.status(400).json({ message: 'Email and password are required' });
@@ -28,8 +28,31 @@ export const createUser = (req, res) => {
 }
 export const getUser = async (req, res) => {
     try {
-        
 
+
+        if (!req.params.id) {
+            return res.status(400).json({ message: 'User ID is required' });
+        }
+        const user = await prisma.user.findUnique({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        return res.status(200).json({ message: 'User found', user });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+export const getUserById = async (req, res) => {
+    session.user = auth(req, res);
+    if (!session.user) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+    try {
         if (!req.params.id) {
             return res.status(400).json({ message: 'User ID is required' });
         }
